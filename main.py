a498258314c7e439892c7daf9aae4730265538e1
juan jose lima <josejuanl_1994@hotmail.es>
2018-09-12 11:14:40 -0600

    python
    lala

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import  Flask,jsonify, abort
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Acceso Correcto',
+        'description': u'Se reconocio la huella',
+        'done': False
+    },
+    {
+        'id': 0,
+        'title': u' Acceso Incorrecto',
+        'description': u'Huella incorrecta',
+        'done': False
+    }
+]
+
+
+@app.route('/prueba/tasks/<int:task_id>', methods=['GET'])
+def get_tasks(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'tasks': task[0]})
+
+
+if __name__ == '__main__':
+    app.run()
